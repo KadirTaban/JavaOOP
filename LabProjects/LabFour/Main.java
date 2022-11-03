e3384573eec0be67e35389ec565c536432eba21d
KadirTaban <kadirtaban08@gmail.com>
2022-11-03 10:41:36 +0300

    test

--- LabProjects/LabFour/Main.java
+++ LabProjects/LabFour/Main.java
@@ -4,30 +4,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String inputName, inputLastname, inputJob;
-        int inputAge;
 
-        Scanner input = new Scanner(System.in);
+        Person person = new Person("Kadir", "Taban",4,"Engineer");        
+        Person person2 = new Person("Omer","Taban",5,"CEO");
 
-        System.out.print(" name: ");
-        inputName = input.nextLine();
-
-        System.out.print(" surname: ");
-        inputLastname = input.nextLine();
-
-        System.out.print(" job: ");
-        inputJob = input.nextLine();
-
-        System.out.print(" age: ");
-        inputAge = input.nextInt();
-
-        Person person = new Person(inputName, inputLastname,inputAge,inputJob);
-
-        person.displayInfo();
-        
         Car car = new Car(person, "Blue", "2000");
 
         car.getOwner().displayInfo();        
+        car.changeOwner(person2);
+        car.getOwner().displayInfo();        
+
     }
     
 }
